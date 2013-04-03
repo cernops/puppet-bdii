28f448af0c90a5aac8552f2f4d2b74a616d25ce9
Ulrich Schwickerath <ulrich.schwickerath@cern.ch>
2013-04-03 16:56:33 +0200

    remove monitoring call from init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,8 @@ class bdii {
          class {'bdii::install':}
          class {'bdii::config':}
          class {'bdii::service':}
-         class {'bdii::lemon':}
          class {'bdii::firewall':}
+    # monitoring is not automatically setup for this module
     }
    default: {
               # There is some fedora configuration present but I can't actually get it to work.
