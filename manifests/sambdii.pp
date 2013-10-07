d709d9ee4af34090e91132ef15ae3dfea0eefe79
Ulrich Schwickerath <Ulrich.Schwickerath@cern.ch>
2013-10-07 16:24:31 +0200

    add bdii type for sam adding fcr

---
+++ manifests/sambdii.pp
@@ -0,0 +1,20 @@
+class bdii::sambdii inherits bdii::params {
+
+  Class[Bdii::Config] -> Class[Bdii::Sambdii]
+
+  package { "emi-bdii-top":
+          ensure => present,
+  }
+  package { "glite-info-plugin-fcr":
+    ensure => present,
+  }
+
+  file {"/etc/bdii/gip/glite-info-site-defaults.conf":
+      content => template('bdii/glite-info-site-defaults.erb'),
+      owner => 'root',
+      group => 'root',
+      mode => '644',
+      loglevel => err,
+  }
+}
+  
