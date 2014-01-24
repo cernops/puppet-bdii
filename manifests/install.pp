66012853300c90ca413ce2a2123f1808e76f759f
fschaer <frederic.schaer@cea.fr>
2014-01-24 17:40:57 +0100

    remove ghost packages (not found anywhere)

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,17 +1,8 @@
 class bdii::install (
-  $selinux = $bdii::params::selinux
 ) inherits bdii::params {
 
   package { bdii:
     ensure => present,
   }
 
-  if $selinux {
-    package { selinux-policy-targeted-emi2-bdii:
-      ensure => present,
-    }   
-    package { selinux-policy-targeted-emi2-hotfixes:
-      ensure => present,
-    }   
-  }
 }
