a84f306c03f0dcf60fe47673177a660a81d6599b
fschaer <frederic.schaer@cea.fr>
2014-03-10 10:54:46 +0100

    allow for inherited parameter override

--- manifests/sitebdii.pp
+++ manifests/sitebdii.pp
@@ -1,4 +1,26 @@
-class bdii::sitebdii inherits bdii::params {
+class bdii::sitebdii(
+  #this allows to override specific params not only relying on hiera, for sites which have subsite BDIIs, whishing to use hiera both for site and subsite
+  $siteurls              =  $bdii::params::siteurls,
+  $sitename              =  $bdii::params::sitename,
+  $sitebdiihost          =  $bdii::params::sitebdiihost,
+  $sitedesc              =  $bdii::params::sitedesc,
+  $siteweb               =  $bdii::params::siteweb,
+  $sitedistributed       =  $bdii::params::sitedistributed,
+  $siteloc               =  $bdii::params::siteloc,
+  $sitecountry           =  $bdii::params::sitecountry,
+  $sitelat               =  $bdii::params::sitelat,
+  $sitelong              =  $bdii::params::sitelong,
+  $siteemail             =  $bdii::params::siteemail,
+  $sitesecuritymail      =  $bdii::params::sitesecuritymail,
+  $sitesupportemail      =  $bdii::params::sitesupportemail,
+  $config                =  $bdii::params::config,
+  $egeeroc               =  $bdii::params::egeeroc,
+  $egeeservice           =  $bdii::params::egeeservice,
+  $grid                  =  $bdii::params::grid,
+  $wlcgtier              =  $bdii::params::wlcgtier,
+  $otherinfo             =  $bdii::params::otherinfo,
+  
+) inherits bdii::params {
 
   Class[Bdii::Config] -> Class[Bdii::Sitebdii]
 
