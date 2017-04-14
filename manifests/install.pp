e832364cb6b3847f91496e0a1791c33578232f8c
fschaer <frederic.schaer@cea.fr>
2017-04-14 11:34:52 +0200

    remove disappeared selinux emi packages setup

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,17 +1,7 @@
-class bdii::install (
-  $selinux = $bdii::params::selinux
-) inherits bdii::params {
+class bdii::install inherits bdii::params {
 
   package { 'bdii':
     ensure => present,
   }
 
-  if $selinux {
-    package { 'selinux-policy-targeted-emi2-bdii':
-      ensure => present,
-    }
-    package { 'selinux-policy-targeted-emi2-hotfixes':
-      ensure => present,
-    }
-  }
 }
