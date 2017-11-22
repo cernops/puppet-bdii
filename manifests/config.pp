6aece3382fe0a6a12ffb3cfccc1ebae34a4feaac
andrea-manzi <andrea.manzi@gmail.com>
2017-11-22 15:12:09 +0100

    fix last lint errors

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,47 +10,47 @@ class bdii::config (
       Class[bdii::install] -> Class[bdii::config]
 
       file {'/etc/bdii/bdii.conf':
-        content => template('bdii/bdiiconf.erb'),
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        notify  => Class["bdii::service"],
+        content  => template('bdii/bdiiconf.erb'),
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0644',
+        notify   => Class['bdii::service'],
         loglevel => $loglevel,
       }
 
       file { '/etc/bdii/gip':
-        ensure => "directory",
-        owner  => "root",
-        group  => "root",
-        mode   => '0755',
+        ensure   => 'directory',
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0755',
         loglevel => $loglevel,
       }
 
       file { '/var/lib/bdii/db':
-        ensure => "directory",
-        owner  => "ldap",
-        group  => "ldap",
-        mode   => '0755',
+        ensure   => 'directory',
+        owner    => 'ldap',
+        group    => 'ldap',
+        mode     => '0755',
         loglevel => $loglevel,
       }
 
       file {'/etc/sysconfig/bdii':
-        content => template('bdii/bdiisysconf.erb'),
-        owner => 'root',
-        group => 'root',
-        mode => '0644',
-        loglevel => $loglevel,    
+        content  => template('bdii/bdiisysconf.erb'),
+        owner    => 'root',
+        group    => 'root',
+        mode     => '0644',
+        loglevel => $loglevel
       }
 
       file_line{ 'slapd_threads':
-        path => $slapdconf,
-        match => "^\s*threads",
-        line => "threads          ${bdii::params::slapdthreads}",
+        path     => $slapdconf,
+        match    => '^\s*threads',
+        line     => "threads          ${bdii::params::slapdthreads}",
       }
 
       file_line{ 'slapd_loglevel':
-        path => $slapdconf,
-        match => "^\s*loglevel",
-        line  => "loglevel       ${bdii::params::slapdloglevel}",
+        path     => $slapdconf,
+        match    => '^\s*loglevel',
+        line     => "loglevel       ${bdii::params::slapdloglevel}",
       }
 }
