e11f4a92d4fa45f959afba4fbf2ceaea44457e3f
Sean Crosby <scrosby@unimelb.edu.au>
2018-04-18 16:09:58 +1000

    Allow the firewall component to not be loaded

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,15 @@
-class bdii {
+class bdii (
+  $manage_firewall = true,
+) {
 
   case $::operatingsystem {
     'RedHat','SLC','SL','Scientific','CentOS':   {
       include bdii::install
       include bdii::service
-      include bdii::firewall
       include bdii::config
+      if ($manage_firewall) {
+        include bdii::firewall
+      }
   }
     default: {
               # There is some fedora configuration present but I can't actually get it to work.
